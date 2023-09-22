8a105a08eea7647d01378bfd2711f6be62bf1e5c
cxk <123qq.com>
2023-09-22 19:48:39 +0800

    first commit

--- idea/Project/git-text/src/com/Test.java
+++ idea/Project/git-text/src/com/Test.java
@@ -4,5 +4,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("holle word");
         System.out.println("git word");
+        System.out.println("HOLle git");
     }
 }
